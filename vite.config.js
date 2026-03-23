f65892b1326cf9f750322d17c46023c15ce2bd35
Kadir Akyürek <kadirakyurek353@gmail.com>
2026-03-23 13:44:32 +0300

    Update vite config dynamically for GH pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/trendyol/',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    base: command === 'build' ? '/trendyol/' : '/',
+  }
 })
